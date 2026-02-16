5571ca26794c2519a23e9d10154b97359c6c88f2
shohan89 <your-maffin.killer007@gmail.com>
2026-02-17 02:22:20 +0600

    Display All products by default on Products page.

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,16 @@
+// Load all poducts
+const loadAllProducts = async () => {
+    showLoading();
+    try {
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data = await res.json();
+        const allProducts = data;
+        displayAllProducts(allProducts);
+    } catch (error) {
+        console.error('Error fetching all products: ', error);
+    }
+}
+
 // Load all categories
 const loadAllCategories = async () => {
     try {
@@ -24,6 +37,49 @@ const hideLoading = () => {
     const categoryProductContainer = document.getElementById('categoryProductContainer');
     spinner.classList.add('hidden');
     categoryProductContainer.classList.remove('hidden');
+}
+
+// Display all products
+const displayAllProducts = allProducts => {
+    
+    const categoryProductContainer = document.getElementById('categoryProductContainer');
+    categoryProductContainer.innerHTML = ''; // clear the previous data
+    allProducts.forEach(product => {
+        const productCardDiv = document.createElement('div');
+        productCardDiv.classList.add('card', 'bg-base-100', 'shadow-sm');
+        productCardDiv.innerHTML = `
+            <figure>
+            <img
+            class="h-[200px] md:h-[500px] w-full"
+              src=${product.image}
+              alt=${product.title}
+            />
+          </figure>
+          <div class="card-body">
+            <div class="body-header flex justify-between py-5">
+              <div class="badge badge-soft badge-primary">${product.category}</div>
+              <div class="reviews">
+                <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate} (${product.rating.count})
+              </div>
+            </div>
+            <h2 class="card-title">${product.title.length > 30 ? product.title.slice(0, 30) + '...' : product.title}</h2>
+            <p>
+              ${product.description.slice(0, 100)}...
+            </p>
+            <h4 class="text-xl font-bold">$${product.price}</h4>
+            <div class="flex gap-4 pt-8">
+              <button class="btn btn-soft flex-1">
+                <i class="fa-regular fa-eye"></i> Details
+              </button>
+              <button class="btn btn-primary flex-1">
+                <i class="fa-solid fa-cart-shopping"></i> Add
+              </button>
+            </div>
+          </div>
+        `;
+        categoryProductContainer.appendChild(productCardDiv);
+    });
+    hideLoading();
 }
 // Display all categories
 const displayCategories = categories => {
@@ -70,7 +126,7 @@ const loadAllProductsByCategory = async (category) => {
 
 // Display category products
 const displayCategoryProducts = allProducts => {
-    
+
     const categoryProductContainer = document.getElementById('categoryProductContainer');
     categoryProductContainer.innerHTML = ''; // clear the product container
     
@@ -112,5 +168,7 @@ const displayCategoryProducts = allProducts => {
     hideLoading(); // hide loading spinner after load the content
 }
 
+
+loadAllProducts();
 loadAllCategories();
 // loadAllProductsByCategory("women's clothing");

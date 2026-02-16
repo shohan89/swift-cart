d1f4c6a3319d891868dcd783d2b3f470d3746b6b
shohan89 <your-maffin.killer007@gmail.com>
2026-02-17 00:24:22 +0600

    Display Category Wise Products on The Console.

--- scripts/products.js
+++ scripts/products.js
@@ -11,7 +11,7 @@ const loadAllCategories = async () => {
 }
 // Display all categories
 const displayCategories = categories => {
-    console.log(categories);
+    // console.log(categories);
     const categoryContainer = document.getElementById('categoryContainer');
     categories.forEach(category => {
         const categoryBtn = document.createElement('button');
@@ -23,21 +23,58 @@ const displayCategories = categories => {
     });
 }
 
-loadAllCategories();
-
-
-
 // Load all poducts by category
 const loadAllProductsByCategory = async (category) => {
     try {
         const res = await fetch(`https://fakestoreapi.com/products/category/${category}`);
         const data = await res.json();
         const allProducts = data;
-        // console.log(allProducts);
+        displayCategoryProducts(allProducts);
     } catch (error) {
         console.error('Error fetching all products: ', error);
     }
 }
 
+// Display category products
+const displayCategoryProducts = allProducts => {
+    console.log(allProducts);
+    const categoryProductContainer = document.getElementById('categoryProductContainer');
+    allProducts.forEach(product => {
+        const productCardDiv = document.createElement('div');
+        productCardDiv.classList.add('card', 'bg-base-100', 'shadow-sm');
+        productCardDiv.innerHTML = `
+            <figure>
+            <img
+            class="h-[200px] md:h-[500px] w-full"
+              src=${product.image}
+              alt=${product.title}
+            />
+          </figure>
+          <div class="card-body">
+            <div class="body-header flex justify-between py-5">
+              <div class="badge badge-soft badge-primary">${product.category}</div>
+              <div class="reviews">
+                <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate} (${product.rating.count})
+              </div>
+            </div>
+            <h2 class="card-title">${product.title.slice(0, 30)}...</h2>
+            <p>
+              ${product.description.slice(0, 100)}...
+            </p>
+            <h4 class="text-xl font-bold">$${product.price}</h4>
+            <div class="flex gap-4 pt-8">
+              <button class="btn btn-soft flex-1">
+                <i class="fa-regular fa-eye"></i> Details
+              </button>
+              <button class="btn btn-primary flex-1">
+                <i class="fa-solid fa-cart-shopping"></i> Add
+              </button>
+            </div>
+          </div>
+        `;
+        categoryProductContainer.appendChild(productCardDiv);
+    });
+}
 
-loadAllProductsByCategory('jewelery');
+loadAllCategories();
+loadAllProductsByCategory("women's clothing");
